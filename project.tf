fc4e4017db3def5255d880abfadb966738a986fc
rajanjoshi <joshirajan999@gmail.com>
2021-10-17 17:44:15 +0530

    cloudresourcemanager

--- project.tf
+++ project.tf
@@ -9,6 +9,11 @@ resource "google_project_service" "run" {
   disable_on_destroy = false
 }
 
+resource "google_project_service" "enable_cloud_resource_manager_api" {
+  service  = "cloudresourcemanager.googleapis.com"
+  disable_on_destroy = false
+}
+
 resource "google_project_service" "iam" {
   service = "iam.googleapis.com"
   disable_on_destroy = false
