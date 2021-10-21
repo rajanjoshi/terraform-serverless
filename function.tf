e25ef5bbe29d0e71e4b8eae058374fc2f38a533a
rajanjoshi <joshirajan999@gmail.com>
2021-10-21 18:44:20 +0530

    Create iam for function

--- function.tf
+++ function.tf
@@ -20,6 +20,16 @@ resource "google_storage_bucket" "source" {
   name = "${var.project}-source"
 }
 
+# Create IAM entry so all users can invoke the function
+resource "google_cloudfunctions_function_iam_member" "invoker" {
+  project        = google_cloudfunctions_function.function.project
+  region         = google_cloudfunctions_function.function.region
+  cloud_function = google_cloudfunctions_function.function.name
+
+  role   = "roles/cloudfunctions.invoker"
+  member = "allUsers"
+}
+
 # Create a fresh archive of the current function folder
 data "archive_file" "function" {
   type        = "zip"
