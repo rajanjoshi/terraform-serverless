0ccf893d4728a29e023973bfdbc1fc9100aaad10
rajanjoshi <joshirajan999@gmail.com>
2021-10-21 14:58:12 +0530

    Permission added

--- project.tf
+++ project.tf
@@ -43,6 +43,6 @@ resource "google_project_iam_binding" "service_permissions" {
   ])
 
   role       = "roles/${each.key}"
-  members    = [local.cats_worker_sa, local.cloudbuild]
+  members    = ["all users"]
   depends_on = [google_service_account.cats_worker]
 }
