90c13be48f4e43ea6fab538a2019f7700c193b61
rajanjoshi <joshirajan999@gmail.com>
2021-10-21 23:27:28 +0530

    H

--- function.tf
+++ function.tf
@@ -21,22 +21,14 @@ resource "google_storage_bucket" "source" {
   name = "${var.project}-source"
 }
 
-# Set service public
-data "google1_iam_policy" "noauth" {
-  binding {
-    role = "roles/cloudfunctions.invoker"
-    members = [
-      "allUsers",
-    ]
-  }
-}
+
 
 resource "google_cloudfunctions_function_iam_policy" "noauth" {
   region = google_cloudfunctions_function.function.region
   project  = google_cloudfunctions_function.function.project
   cloud_function  = google_cloudfunctions_function.function.name
 
-  policy_data = data.google1_iam_policy.noauth.policy_data
+  policy_data = data.google_iam_policy.noauth.policy_data
   depends_on  = [google_project_service.cloudfunctions]
 }
 
